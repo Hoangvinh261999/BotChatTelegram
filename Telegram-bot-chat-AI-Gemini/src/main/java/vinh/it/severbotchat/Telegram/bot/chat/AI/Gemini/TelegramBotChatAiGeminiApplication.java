d012df983721b725b86ffe34e9e83c1553b6c4d6
Nguyễn Hoàng Vinh <96643414+Hoangvinh261999@users.noreply.github.com>
2025-01-13 14:02:47 +0700

    First commit

---
+++ Telegram-bot-chat-AI-Gemini/src/main/java/vinh/it/severbotchat/Telegram/bot/chat/AI/Gemini/TelegramBotChatAiGeminiApplication.java
@@ -0,0 +1,13 @@
+package vinh.it.severbotchat.Telegram.bot.chat.AI.Gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotChatAiGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotChatAiGeminiApplication.class, args);
+	}
+
+}
---
+++ Telegram-bot-chat-AI-Gemini/src/test/java/vinh/it/severbotchat/Telegram/bot/chat/AI/Gemini/TelegramBotChatAiGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package vinh.it.severbotchat.Telegram.bot.chat.AI.Gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotChatAiGeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
